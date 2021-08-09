029f081de35f24cc00c9576bac96ea7342805a75
Conner Gleason <connereg@optonline.net>
2021-08-09 13:00:16 -0400

    commit tree 522864a339128ab1fd1476904e21763e550a151d

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+let feet;
+
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  
+  if (feet <= 400){
+    return 'This one is on me!'
+  }
+  else if (feet <= 2499){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet >= 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const isNewYorkCity = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return isNewYorkCity
+
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+  switch (tipAmount) {
+    case 'generous':
+      const generousTipResponse = "Thank you so much."
+      return generousTipResponse;
+      break;
+    case 'not as generous':
+      const notGenerousTipResponse = "Thank you."
+      return notGenerousTipResponse;
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
+
 }
